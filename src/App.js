987f85fe563e00863f0cf66b42d44a42ff004b70
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-13 18:41:22 +0530

    added dineout options to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Index } from './components/footer/Index';
 import { Explore } from './components/exploreOptions/Explore';
 import { Common } from './components/common /Common';
 import { Sliderr } from './components/carousel/Sliderr';
+import { Dinning } from './components/dinningOut/Dinning';
 
 function App() {
   return (
     <div className='App'>
       <Common />
       <Sliderr />
+      <Dinning />
       <Explore />
       <Index />
     </div>
---
+++ src/components/dinningOut/Dinning.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Dinning = () => {
+  return (
+    <div className="dineout">
+        <ul className="food-list">
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+            <li>food</li>
+        </ul>
+    </div>
+  )
+}
