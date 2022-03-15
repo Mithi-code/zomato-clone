e3d4e33aafa2ad7eb90713ed626702df1ccb60a9
mithlesh kumar <mithlesh.9m@gmail.com>
2022-03-15 18:46:35 +0530

    added common section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Index } from './components/footer/Index';
 import { Explore } from './components/exploreOptions/Explore'
+import { Common } from './components/common /Common'
 
 function App() {
   return (
     <div className="App">
+      <Common />
       <Explore />
       <Index />
     </div>
