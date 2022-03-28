7b47a4fdf8bab8ec328a7ddcd487456c0511185d
mithlesh kumar <mithlesh.9m@gmail.com>
2022-03-28 22:03:59 +0530

    list

--- src/components/nightLife/night.js
+++ src/components/nightLife/night.js
@@ -19,6 +19,14 @@ export const night = () => {
         <li>7</li>
         <li>7</li>
       </ul>
+      <ul>
+        <li>7</li>
+        <li>7</li>
+        <li>7</li>
+        <li>7</li>
+        <li>7</li>
+        <li>7</li>
+      </ul>
     </div>
   )
 }
